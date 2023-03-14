160fb416e6fafc4ad090ce35dba3d70f6630eacd
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2023-03-14 16:04:24 +0100

    uploading in batches

--- app/routes/api/station/upload-data.ts
+++ app/routes/api/station/upload-data.ts
@@ -3,18 +3,20 @@ import {prisma} from "~/db.server";
 import {z} from "zod";
 import {parseJsonRequest} from "~/validation.server";
 import {json} from "@remix-run/node";
+import type {Prisma} from "@prisma/client";
+import { PrismaClientKnownRequestError} from "@prisma/client/runtime/library";
 
 const schema = z.object({
     token: z.string().cuid(),
-    data: z.object({
+    data: z.array(z.object({
         temperature: z.number(),
         humidity: z.number(),
         date: z.coerce.date()
-    })
+    })).min(1).max(100)
 })
 
 export const action = (async ({request}) => {
-    const {data, token} = await parseJsonRequest(request, schema);
+    const {data: batchData, token} = await parseJsonRequest(request, schema);
     const station = await prisma.station.findUnique({where: {token}});
     if (!station) {
         throw new Response("Station not found", {status: 404});
@@ -22,14 +24,18 @@ export const action = (async ({request}) => {
     if (station.state == "INACTIVE") {
         throw new Response("Station is inactive", {status: 400});
     }
-    await prisma.stationData.create({
-        data: {
-            stationId: station.id,
-            id: data.date,
-            humidity: data.humidity,
-            temperature: data.humidity
+    const data = batchData.map(batch => (
+        {stationId: station.id, id: batch.date, humidity: batch.humidity, temperature: batch.temperature}
+    ) satisfies Prisma.StationDataCreateManyInput)
+    try {
+        await prisma.stationData.createMany({data})
+    } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError && e.code === "P2002") {
+            // TODO figure out which id has conflict
+            throw new Response("Data at that time already saved", {status: 400})
         }
-    })
+        throw e
+    }
     return json({
         result: "Data uploaded",
         state: station.state
