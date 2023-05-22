dbaff2f0427523636b275574addf8f01f642be96
Adam Tretera <adam.tretera@ackee.cz>
2023-05-22 18:06:45 +0200

    :lipstick: Push busin

--- src/components/charts/chart.tsx
+++ src/components/charts/chart.tsx
@@ -7,13 +7,10 @@ import {
     Title,
     Tooltip,
     Legend,
+    ChartData,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Card } from '@mantine/core';
-
-import { formatDate } from './utils';
-
-import { chartData } from '~/components/charts/utils/mock';
 
 ChartJS.register(
     CategoryScale,
@@ -25,7 +22,11 @@ ChartJS.register(
     Legend
 );
 
-export const Chart = () => {
+interface IChartProps {
+    data: ChartData<'line', number[], string>;
+}
+
+export const Chart = ({ data }: IChartProps) => {
     const options = {
         responsive: true,
         interaction: {
@@ -34,21 +35,8 @@ export const Chart = () => {
         },
     } as const;
 
-    const data = {
-        labels: chartData.map((item) => formatDate(new Date(item.datetime))),
-        datasets: [
-            {
-                label: 'Humidity',
-                data: chartData.map((item) => item.humidity),
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                yAxisID: 'y',
-            },
-        ],
-    };
-
     return (
-        <Card>
+        <Card withBorder shadow={'sm'}>
             <Line width={'100%'} height={100} data={data} options={options} />
         </Card>
     );
--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 import { createFormContext } from '@mantine/form';
-import { Grid, Loader } from '@mantine/core';
+import { Flex, Grid, Loader, useMantineTheme } from '@mantine/core';
 
 import { Controls } from '~/components/controls';
 import { dataSchema, StationDataGranularity } from '~/utils/validations';
@@ -24,24 +24,82 @@ export const [FormProvider, useFormContext, useForm] =
 interface IDashboardProps {
     stationId: string;
 }
+interface Data {
+    value: number;
+    datetime: number;
+}
+
+const humidity: Data[] = [
+    { value: 60, datetime: 1621674000 },
+    { value: 62, datetime: 1621677600 },
+    { value: 58, datetime: 1621681200 },
+    { value: 55, datetime: 1621684800 },
+    { value: 65, datetime: 1621688400 },
+];
+
+const temperature: Data[] = [
+    { value: 25, datetime: 1621674000 },
+    { value: 26, datetime: 1621677600 },
+    { value: 24, datetime: 1621681200 },
+    { value: 22, datetime: 1621684800 },
+    { value: 27, datetime: 1621688400 },
+];
+
 export const Dashboard = ({ stationId }: IDashboardProps) => {
+    const theme = useMantineTheme();
+
     const form = useForm({
         initialValues: initialControls,
     });
 
-    const {
-        isLoading,
-        isError,
-        error,
-        data: locations,
-    } = api.stationData.getDataForStationById.useQuery({
-        stationId,
-        ...form.values,
-    });
+    const { isLoading, isError, error, data } =
+        api.stationData.getDataForStationById.useQuery({
+            stationId,
+            ...form.values,
+        });
 
-    if (isLoading) {
-        return <Loader />;
-    }
+    /*   const humidity = data?.map(({ humidity: value, datetime }) => ({
+        value,
+        datetime,
+    }));
+
+    const temperature = data?.map(
+        ({ temperature: value, humidity, datetime }) => ({ value, datetime })
+    );*/
+
+    const humidityChartData = {
+        labels:
+            humidity?.map(({ datetime }) =>
+                new Date(datetime).toLocaleDateString()
+            ) ?? [],
+        datasets: [
+            {
+                label: 'Humidity',
+                data: humidity?.map(({ value }) => value) ?? [],
+                borderColor: theme.colors.blue[5],
+                backgroundColor: theme.colors.blue[6],
+                yAxisID: 'y',
+                tension: 0.3,
+            },
+        ],
+    };
+
+    const temperatureChartData = {
+        labels:
+            temperature?.map(({ datetime }) =>
+                new Date(datetime).toLocaleDateString()
+            ) ?? [],
+        datasets: [
+            {
+                label: 'Temperature',
+                data: temperature?.map(({ value }) => value) ?? [],
+                borderColor: theme.colors.red[5],
+                backgroundColor: theme.colors.red[6],
+                yAxisID: 'y',
+                tension: 0.3,
+            },
+        ],
+    };
 
     if (isError) {
         return <div>{error.message}</div>;
@@ -50,14 +108,20 @@ export const Dashboard = ({ stationId }: IDashboardProps) => {
     return (
         <FormProvider form={form}>
             <Controls />
-            <Grid pt={20}>
-                <Grid.Col span={6}>
-                    <Chart />
-                </Grid.Col>
-                <Grid.Col span={6}>
-                    <Chart />
-                </Grid.Col>
-            </Grid>
+            {isLoading ? (
+                <Flex justify={'center'} p={20}>
+                    <Loader />
+                </Flex>
+            ) : (
+                <Grid pt={20}>
+                    <Grid.Col span={6}>
+                        <Chart data={humidityChartData} />
+                    </Grid.Col>
+                    <Grid.Col span={6}>
+                        <Chart data={temperatureChartData} />
+                    </Grid.Col>
+                </Grid>
+            )}
         </FormProvider>
     );
 };
