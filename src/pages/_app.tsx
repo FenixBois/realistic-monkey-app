7efab0f1c8d8e0ba1d30d221853c4db665c8284b
PeanutButte7 <adam.barta007@gmail.com>
2023-05-22 17:20:50 +0200

    :sparkles: Add color scheme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,25 +5,32 @@ import { SessionProvider } from 'next-auth/react';
 import { api } from '~/utils/api';
 
 import '~/styles/globals.css';
-import { MantineProvider } from '@mantine/core';
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-    Component,
-    pageProps: { session, ...pageProps },
-}) => {
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+    const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+    const toggleColorScheme = (value?: ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
     return (
-        <SessionProvider session={session}>
-            <MantineProvider
-                withNormalizeCSS
-                withGlobalStyles
-                theme={{ colorScheme: 'light', primaryColor: 'yellow' }}
-            >
-                <Component {...pageProps} />
-                <ReactQueryDevtools initialIsOpen={true} />
-            </MantineProvider>
-        </SessionProvider>
+        <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
+        >
+            <SessionProvider session={session}>
+                <MantineProvider
+                    withNormalizeCSS
+                    withGlobalStyles
+                    theme={{ colorScheme, primaryColor: 'yellow' }}
+                >
+                    <Component {...pageProps} />
+                    <ReactQueryDevtools initialIsOpen={true} />
+                </MantineProvider>
+            </SessionProvider>
+        </ColorSchemeProvider>
     );
 };
 
