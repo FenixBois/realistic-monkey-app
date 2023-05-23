6685424608eae956c146061406ebd1e5c4497b1d
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2023-05-23 05:44:59 +0200

    epic seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,82 +1,85 @@
-import {
-    PrismaClient,
-    StationState,
-} from '@prisma/client';
+import {PrismaClient, StationState,} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
     const location1 = await prisma.location.create({
+        include: {stations: true},
         data: {
-            name: 'Location 1',
+            name: 'Parukářka',
             stations: {
                 create: [
                     {
-                        name: 'Station 1',
-                        gatewayInfo: 'Gateway 1',
+                        name: 'Einstein',
+                        gatewayInfo: 'Parukářka Einstein',
                         state: StationState.ACTIVE,
-                        data: {
-                            create: [
-                                {
-                                    humidity: 50.0,
-                                    temperature: 20.0,
-                                },
-                            ],
-                        },
+                        lastReceivedDataAt: new Date(2023, 4, 23, 10, 0, 0, 0)
                     },
                     {
-                        name: 'Station 2',
-                        gatewayInfo: 'Gateway 2',
+                        name: 'Lobby',
+                        gatewayInfo: 'Parukářka Lobby',
                         state: StationState.REGISTERED,
-                        data: {
-                            create: [
-                                {
-                                    humidity: 40.0,
-                                    temperature: 15.0,
-                                },
-                            ],
-                        },
                     },
                 ],
             },
         },
     });
+    const einstein = location1.stations.find(s => s.name === "Einstein")!
 
     const location2 = await prisma.location.create({
+        include: {stations: true},
         data: {
-            name: 'Location 2',
+            name: 'Kolbenova',
             stations: {
                 create: [
                     {
-                        name: 'Station 3',
-                        gatewayInfo: 'Gateway 3',
-                        state: StationState.REGISTERED,
-                        data: {
-                            create: [
-                                {
-                                    humidity: 50.0,
-                                    temperature: 20.0,
-                                },
-                            ],
-                        },
+                        name: '1.03',
+                        gatewayInfo: 'Kolbenova 1.03',
+                        state: StationState.INACTIVE,
+                        lastReceivedDataAt: new Date(2022, 6, 23, 10, 0, 0, 0)
                     },
                     {
-                        name: 'Station 4',
-                        gatewayInfo: 'Gateway 4',
-                        state: StationState.INACTIVE,
-                        data: {
-                            create: [
-                                {
-                                    humidity: 40.0,
-                                    temperature: 15.0,
-                                },
-                            ],
-                        },
-                    },
-                ],
-            },
+                        name: '1.01',
+                        gatewayInfo: 'Kolbenova 1.01',
+                        state: StationState.ACTIVE,
+                        lastReceivedDataAt: new Date(2023, 4, 23, 10, 0, 2, 0)
+                    }
+                ]
+            }
         },
     });
+    const kolbenova101 = location2.stations.find(s => s.name === "1.01")!
+
+    const minTemp = 15
+    const maxTemp = 30
+    const minHumidity = 0
+    const maxHumidity = 70
+    const maxHumDiffPerMinute = 5
+
+    const maxDate = new Date(2023, 4, 23, 10, 0, 2, 0);
+
+    const lastData = [
+        {temperature: 25, humidity: 60, stationId: einstein.id},
+        {temperature: 15, humidity: 30, stationId: kolbenova101.id}
+    ];
+
+    let date = new Date(2023, 4, 19, 10, 0, 0, 0);
+    while (date < maxDate) {
+        for (let lastIndex in lastData) {
+            const {temperature: lastTemp, humidity: lastHum, stationId} = lastData[lastIndex]!;
+
+
+            const temperature = Math.min(maxTemp, Math.max(minTemp, lastTemp + (Math.random() - 0.5)));
+            const humidity = Math.min(maxHumidity, Math.max(minHumidity, lastHum + (Math.random() - 0.5) * maxHumDiffPerMinute/2))
+
+            await prisma.stationData.create({
+                data: {stationId,temperature, humidity, datetime: date}
+            })
+
+            lastData[lastIndex] = {temperature, humidity, stationId};
+        }
+        date.setMinutes(date.getMinutes() + 1);
+    }
 }
 
 main()
